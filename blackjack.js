c61ef5f5251b918cfcc0249973f572cf4db7ba55
Lindsey Adams <linzma87@gmail.com>
2016-09-28 21:51:23 -0400

    With Ace 1/11

--- blackjack.js
+++ blackjack.js
@@ -1,16 +1,13 @@
 function runGame() {
-debugger;
+    debugger;
     var cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
     var display = document.getElementById('cards');
 
     function hit() {
-
         var card = (Math.round(Math.random() * cards.length));
         display.innerHTML = display.innerHTML + " " + cards[card];
         checkResult(false, true);
-
     }
-
     /**
      * Check the result of the current cards and alert the game result
      *
@@ -21,22 +18,21 @@ debugger;
     function checkResult(standing, hitting) {
 
         var newCards = display.innerHTML.split(' ');
-        var hitCard = 0;
         var cardValue = 0;
         var index = 0;
 
         newCards.forEach(function(card, index) {
 
             if (card === 'J' || card === 'Q' || card === 'K') {
-
                 cardValue = cardValue + 10;
-
             } else if (card === "A") {
-
-                cardValue = cardValue + 11;
-
+                var aceValue = Math.round(Math.random());
+                if (aceValue === 0) {
+                    cardValue = cardValue + 1;
+                } else if (aceValue === 1) {
+                    cardValue = cardValue + 11;
+                }
             } else {
-
                 card = Number(card);
                 cardValue = cardValue + card;
             }
@@ -44,26 +40,23 @@ debugger;
         });
 
         if (cardValue < 15 && standing) {
-
             alert('Dealer wins.');
+            display.innerHTML = '';
+            runGame();
         } else if (cardValue < 18 && standing) {
-
             alert('Push!');
+            display.innerHTML = '';
+            runGame();
         } else if (cardValue > 18 && standing || cardValue === 21) {
-
             alert('You win!');
+            display.innerHTML = '';
+            runGame();
         } else if (cardValue > 21) {
             alert('You Bust.');
-        } else if (hitting) {
-
-          hitCard = Math.round(Math.random() * cards.length);
-
+            display.innerHTML = '';
+            runGame();
         }
-        // display.innerHTML = '';
-        // display.innerHTML = display.innerHTML;
-        // display.innerHTML = cards[card];
     }
-
 
     document.getElementById('stand').addEventListener('click', function() {
         checkResult(true, false);
